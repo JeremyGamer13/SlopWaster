0ad68f5f0337f59ab452070c7af87eb8aa5ad206
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2026-05-29 03:05:57 -0600

    move prompts to enums, move things, and add TODOs

--- src/enums.js
+++ src/enums.js
@@ -8,4 +8,111 @@ const Enums = {};
  */
 Enums.OLLAMA_CHAT_ID = "chat";
 
+/**
+ * The built-in system prompt for the Agent model.
+ * The Agent model is responsible for being the interpreter and communicator between the user and the rest of the AI models.
+ * It interprets the prompt and determines what aspects need to be implemented, while also explaining this to the user.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_AGENT = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "You are not responsible for generating the application itself. You are the bridge between the user and the internal system tooling."
+    + "\n" + "Despite this, you must present yourself as the creation tool for ease-of-use."
+    + "\n"
+    + "\n" + "The underlying systems will always generate JavaScript + HTML5 browser code. No other languages are supported."
+    + "\n" + "Despite your goal as an assistant; do not ask for clarification or inquire questions asking how to help."
+    + "\n" + "If the user input is vague, generic, or empty, you must provide a generic, minigame specification."
+    + "\n" + "You may pad the response with greetings or pleasantries to inform the user about any details."
+    + "\n" + "You are allowed to make simple demo tools if the provided instructions cannot correlate with a game."
+    + "\n"
+    + "\n" + "You will be harshly penalized for generating code or discussing programming details about the described application."
+    + "\n" + "If a user asks for specific code or the usage of code, politely decline and describe the underlying aspect introduced by the code instead."
+    + "\n" + "Avoid mentioning specific technical details about application implementation or system specifications."
+    + "\n"
+    + "\n" + "You will be rewarded for describing individual, general details about the application such as:"
+        + " " + "general theme/genre; game mechanics; input methods; visual/audio details; win/loss conditions; and instructions."
+    + "\n" + "Format all application details in bullet points. Assume touch/pointer controls when a control method is not specified."
+    + "\n"
+    + "\n" + "All messages will result in a generated application. No matter what you respond with, an application will be generated based on what has been discussed."
+    + "\n" + "You will now chat with the user to create the application. Once your message ends, the application will automatically be attached.";
+/**
+ * The built-in system prompt for the Translator model.
+ * The Translator model is responsible for turning the Agent response into technical details for implementation.
+ * It should apply universal concepts like logic flow and simplify overscoped ideas.
+ * In most cases, it should also describe which libraries will be required for the application.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_TRANSLATOR = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+
+/**
+ * The built-in system prompt for the Image Outliner model.
+ * The Image Outliner model is responsible for determining which image assets will be needed for the described program by the Agent response.
+ * It should allow the model to create a structured list of PNG and SVG assets.
+ *  
+ * The intended structured output is:
+ * `{ png:string[], svg:string[] }`
+ * with the arrays being arbitrary mini-descriptions of the image asset necessary.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_OUTLINER_IMAGES = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+/**
+ * The built-in system prompt for the Audio Outliner model.
+ * The Audio Outliner model is responsible for determining which audio assets will be needed for the described program by the Agent response.
+ * It should allow the model to create a structured list of audio assets.
+ *  
+ * The intended structured output is:
+ * `{ audio:string[] }`
+ * with the array being arbitrary mini-descriptions of the audio asset necessary.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_OUTLINER_AUDIO = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+/**
+ * The built-in system prompt for the SVG Generator model.
+ * The SVG Generator model is responsible for creating the SVG assets outlined by the Image Outliner model.
+ * Ideally this prompt should try to enforce strict SVG-only output, with no response padding.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_SVG_GENERATOR = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+
+/**
+ * The built-in system prompt for the JavaScript Detailer model.
+ * The JavaScript Detailer model is responsible for turning the Translator model response into technical JavaScript details for implementation.
+ * It should detail what is necessary in the HTML & how to implement things like the keyboard or touch input in JavaScript.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_JAVASCRIPT_DETAILER = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+// TODO: Do we need a CSS detailer? Might be ideal to make sure that CSS stays consistent on iteration
+
+/**
+ * The built-in system prompt for the Coder model.
+ * The Coder model is actually responsible for generating all of the code necessary for the playable.
+ * 
+ * This prompt is intended for the HTML generation step.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_CODER_HTML = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+/**
+ * The built-in system prompt for the Coder model.
+ * The Coder model is actually responsible for generating all of the code necessary for the playable.
+ * 
+ * This prompt is intended for the CSS generation step. The generated HTML is also included in the chat for context.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_CODER_CSS = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+/**
+ * The built-in system prompt for the Coder model.
+ * The Coder model is actually responsible for generating all of the code necessary for the playable.
+ * 
+ * This prompt is intended for the JavaScript generation step. The generated HTML is also included in the chat for context.
+ * @enum {string}
+ */
+Enums.OLLAMA_PROMPT_CODER_JAVASCRIPT = "You are the talking agent for a minigame/application creation tool."
+    + "\n" + "text.";
+
 module.exports = Enums;
--- src/generator.js
+++ src/generator.js
@@ -11,6 +11,7 @@ const Enums = require("./enums");
  * @type {Generator}
  */
 class Generator {
+    // TODO: Make a type for the input to the constructor, and make it an object. This is so Ollama stuff can be created at the start.
     /**
      * Create a generator for SlopWaster.
      * This generator should be configured if necessary.
@@ -28,30 +29,12 @@ class Generator {
         /** @type {string?} The output folder to store the playable, and any agent data. Must be an absolute path, and must be set before generation. */
         this.outputFolder = outputFolder;
 
+        // TODO: Move this around so Ollama stuff is easier to config and isn't handled all messy (_ollamaClientInstance is really ugly). Likely just make the constructor use an object for initialization and create the ollama clients in the constructor
         /** @type {SlopWasterTypes.AgentPlayableMessage[]} Chat history with a central agent, intended to make the generation process easily interactable. Adjust to use your own system prompt. */
         this.ollamaHistory = [
             {
                 role: "system",
-                content: "You are the talking agent for a minigame/application creation tool."
-                    + "\n" + "You are not responsible for generating the application itself. You are the bridge between the user and the internal system tooling."
-                    + "\n" + "Despite this, you must present yourself as the creation tool for ease-of-use."
-                    + "\n"
-                    + "\n" + "The underlying systems will always generate JavaScript + HTML5 browser code. No other languages are supported."
-                    + "\n" + "Despite your goal as an assistant; do not ask for clarification or inquire questions asking how to help."
-                    + "\n" + "If the user input is vague, generic, or empty, you must provide a generic, minigame specification."
-                    + "\n" + "You may pad the response with greetings or pleasantries to inform the user about any details."
-                    + "\n" + "You are allowed to make simple demo tools if the provided instructions cannot correlate with a game."
-                    + "\n"
-                    + "\n" + "You will be harshly penalized for generating code or discussing programming details about the described application."
-                    + "\n" + "If a user asks for specific code or the usage of code, politely decline and describe the underlying aspect introduced by the code instead."
-                    + "\n" + "Avoid mentioning specific technical details about application implementation or system specifications."
-                    + "\n"
-                    + "\n" + "You will be rewarded for describing individual, general details about the application such as:"
-                    + " " + "general theme/genre; game mechanics; input methods; visual/audio details; win/loss conditions; and instructions."
-                    + "\n" + "Format all application details in bullet points. Assume touch/pointer controls when a control method is not specified."
-                    + "\n"
-                    + "\n" + "All messages will result in a generated application. No matter what you respond with, an application will be generated based on what has been discussed."
-                    + "\n" + "You will now chat with the user to create the application. Once your message ends, the application will automatically be attached.",
+                content: Enums.OLLAMA_PROMPT_AGENT,
             },
         ];
 
@@ -112,6 +95,43 @@ class Generator {
         }
     }
 
+    /**
+     * Generate a playable based on a prompt. Adjust `ollamaHistory` to use your own system prompt.
+     * 
+     * Should only be used if the `outputFolder` is empty or contains no agent data.
+     * If the `outputFolder` already contains data, the `iterate` method should be used instead.
+     * 
+     * If this method fails, empty out the `outputFolder` before re-running.
+     * @param {string} prompt The prompt to generate the game off of.
+     * @returns {SlopWasterTypes.AgentPlayableResponse}
+     */
+    async generate(prompt) {
+        // initiate the process
+        await this._makeFolderStructure();
+        this._ollamaClient.overwriteChat(Enums.OLLAMA_CHAT_ID, this.ollamaHistory);
+
+        const response = await this._ollamaClient.chatPrompt(Enums.OLLAMA_CHAT_ID, prompt);
+        return {
+            role: "assistant",
+            content: response.content,
+            thinking: response.thinking,
+            playable: this.outputPlayable,
+        }
+    }
+    // TODO: add iteration, likely add the technical & "javascript detail" generations to a chat and, in a new message, tell it to redo them based on the reiterated Agent message + technical detail message
+    /**
+     * Iterate on the playable based on a prompt. Previous agent messages are stored in `ollamaHistory`.
+     * The underlying internal ML model interactions are stored within the `agentFolder`.
+     * 
+     * Should only be used if the `outputFolder` already contains agent data.
+     * @param {string} prompt The prompt to generate the game off of.
+     * @returns {SlopWasterTypes.AgentPlayableResponse}
+     */
+    async iterate(prompt) {
+        await this._makeFolderStructure();
+        throw new Error("Not implemented");
+    }
+
     // internal stuff
     /** @private @type {import("ollama-chatting")} */
     get _ollamaClient() {
@@ -139,42 +159,6 @@ class Generator {
         } catch (err) {
             throw err;
         }
-    }
-
-    /**
-     * Generate a playable based on a prompt. Adjust `ollamaHistory` to use your own system prompt.
-     * 
-     * Should only be used if the `outputFolder` is empty or contains no agent data.
-     * If the `outputFolder` already contains data, the `iterate` method should be used instead.
-     * 
-     * If this method fails, empty out the `outputFolder` before re-running.
-     * @param {string} prompt The prompt to generate the game off of.
-     * @returns {SlopWasterTypes.AgentPlayableResponse}
-     */
-    async generate(prompt) {
-        // initiate the process
-        await this._makeFolderStructure();
-        this._ollamaClient.overwriteChat(Enums.OLLAMA_CHAT_ID, this.ollamaHistory);
-
-        const response = await this._ollamaClient.chatPrompt(Enums.OLLAMA_CHAT_ID, prompt);
-        return {
-            role: "assistant",
-            content: response.content,
-            thinking: response.thinking,
-            playable: this.outputPlayable,
-        }
-    }
-    /**
-     * Iterate on the playable based on a prompt. Previous agent messages are stored in `ollamaHistory`.
-     * The underlying internal ML model interactions are stored within the `agentFolder`.
-     * 
-     * Should only be used if the `outputFolder` already contains agent data.
-     * @param {string} prompt The prompt to generate the game off of.
-     * @returns {SlopWasterTypes.AgentPlayableResponse}
-     */
-    async iterate(prompt) {
-        await this._makeFolderStructure();
-        throw new Error("Not implemented");
     }
 }
 
